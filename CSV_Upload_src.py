e64d2019939ba811c79495cd3f8882d69129ff5f
mattblasa <blasa.matthew@yahoo.com>
2021-02-01 18:13:21 -0600

    Update CSV_Upload_src.py

--- CSV_Upload_src.py
+++ CSV_Upload_src.py
@@ -4,7 +4,7 @@ import os
 from sqlalchemy import create_engine
 
 #create a dataframe
-df = pd.read_csv("test,csv", encoding ='utf8')
+df = pd.read_csv("test.csv", encoding ='utf8')
 
 #create an engine 
 engine = create_engine('postgresql://postgres:noyS9oud!@localhost:5432/dbo.examples.marketing_practice')
